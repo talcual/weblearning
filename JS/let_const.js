75a3f41a84aee64a1b407afe2a2a2b09cdaf0aad
Luis Toscano <commentariosweb@gmail.com>
2025-08-13 01:05:39 +0000

    nuevos temas de nodejs

--- JS/let_const.js
+++ JS/let_const.js
@@ -1,6 +1,18 @@
 
+
+var apellido = 'Toscano 
+gdhfdghsd hhfgfhfhg fhg'
+
 let nombre = "Luis"; // valor que puede cambiar
 nombre = "Fernando";
 
 const PI = 3.1416; // valor que no cambia
 // PI = 3.15; ❌ Error
+
+let salon = '01'
+
+if(salon == '01'){
+    let salon = 'A34'
+}
+
+console.log(salon);
